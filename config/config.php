685b36d7b3a735d578006951214232d541b1731f
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2021-03-02 17:03:58 +0100

    Add comments to the documentation

--- config/config.php
+++ config/config.php
@@ -15,7 +15,14 @@ return [
 
     'settings' => [
         'commonmark' => [
-            //
+            // Check out the official docs for possible configuration options:
+            // https://commonmark.thephpleague.com/1.6/configuration/
+
+            // 'renderer' => [
+            //     'block_separator' => "\n",
+            //     'inner_separator' => "\n",
+            //     'soft_break'      => "\n",
+            // ],
         ],
     ],
 
