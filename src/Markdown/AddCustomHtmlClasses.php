0a1a634e6e15c0264dba03a9075d0492529dde41
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2021-08-19 13:14:26 +0200

    Allow nested selectors (#12)
    This PR allows for nested selectors. You can for example only target p-tags inside a list:
    
     ```php
     'li p' => 'my-css-class', 
    ```
    
    This wasn't possible before.

--- src/Markdown/AddCustomHtmlClasses.php
+++ src/Markdown/AddCustomHtmlClasses.php
@@ -4,8 +4,19 @@ namespace VV\Markdown\Markdown;
 
 class AddCustomHtmlClasses
 {
+    /**
+     * The content the class will parse.
+     */
     public string $content;
+
+    /**
+     * The chosen style which normaly would be `default`.
+     */
     public string $style;
+
+    /**
+     * The belonging style stet as defined inside the config file.
+     */
     public array $styleSet;
 
     public function __construct(string $content, string $style)
@@ -21,29 +32,56 @@ class AddCustomHtmlClasses
      */
     public function handle(): string
     {
-        foreach ($this->styleSet as $tag => $class) {
-            $this->content = str_replace($this->tagFilter($tag), $this->replaceTag($tag, $class), $this->content);
-        }
+        // Fetching all style sets and ordering them after the count.
+        $tags = collect($this->getStyleSet())
+            ->map(fn ($classes, $tags) => new Tag($tags, $classes))
+            ->sortByDesc('count');
+
+        // Parse the content for every defined style set.
+        $tags->each(function ($tag) {
+            $this->content = $this->parse($tag, $this->content);
+        });
 
         return $this->content;
     }
 
     /**
-     * Build the string we want to replace.
+     * Is parsing the content and will add classes to the html-tags.
      */
-    private function tagFilter(string $tag): string
+    public function parse(Tag $tag, string $value): string
     {
-        return "<{$tag}";
+        return preg_replace(
+            $this->defineRegexPattern($tag),
+            $this->defineReplacement($tag),
+            $value
+        );
+    }
+
+    /*
+     * Defines the regex pattern and does take nested selectors into account.
+     */
+    private function defineRegexPattern(Tag $tag): string
+    {
+        $pattern = '';
+
+        foreach ($tag->before as $name) {
+            $pattern .= "<{$name}[^>]*>[^<]*";
+        }
+
+        return "/({$pattern})(<{$tag->tag})(?! class)/iU";
     }
 
     /**
-     * Replace the filtered tag and add custom css classes.
+     * Does add the needed classes into the html tag.
      */
-    private function replaceTag(string $tag, string $class): string
+    private function defineReplacement(Tag $tag): string
     {
-        return "<{$tag} class=\"{$class}\"";
+        return "$1<{$tag->tag} class=\"{$tag->classes}\"";
     }
 
+    /**
+     * Get the style sets from the config for the chosen style set.
+     */
     private function getStyleSet(): array
     {
         $configPath = 'markdown.styles.'.$this->style;
